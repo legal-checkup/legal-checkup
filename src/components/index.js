c6779a5a46addff727fa7dfda250bb2f4143ab07
Reuben Berghan <reuben.berghan@gmail.com>
2018-07-15 23:38:45 +1200

    Update for  NavButton ProgressNav and Question components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 import Footer from './Footer'
 import Header from './Header'
 import Layout from './Layout'
+import NavButton from './NavButton'
 import AnswerBar from './AnswerBar'
 import ProgressBar from './ProgressBar'
+import ProgressNav from './ProgressNav'
+import Question from './Question'
 import QuestionBox from './QuestionBox'
 import TestButton from './TestButton'
 
@@ -10,8 +13,11 @@ export {
   Footer,
   Header,
   Layout,
+  NavButton,
   AnswerBar,
   ProgressBar,
+  ProgressNav,
+  Question,
   QuestionBox,
   TestButton
 }
