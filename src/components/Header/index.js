c773dd46902d6973be37ffc30907a89e52fea583
YWHo <YWHo@users.noreply.github.com>
2018-07-16 12:33:20 +1200

    intial navigation button for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledNavButton = styled.button`
+  color: #ff0000;
+  font-size: 16px;
+  margin: 4px 10px;
+`
+
+StyledNavButton.defaultName = 'StyledNavButton'
 
 export default function Header () {
-  return <header>Header</header>
+  return (
+    <header>
+      <h1>Header</h1>
+      <h2>hello world!</h2>
+      <StyledNavButton>page1</StyledNavButton>
+      <StyledNavButton>page2</StyledNavButton>
+      <StyledNavButton>page3</StyledNavButton>
+    </header>
+  )
 }
