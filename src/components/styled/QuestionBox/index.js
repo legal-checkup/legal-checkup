d8be7280fa57b01a18a636ceb0258b432373c176
Carla Weiss <carlacfw@gmail.com>
2018-07-16 12:00:12 +1200

    added styled question box

---
+++ src/components/styled/QuestionBox/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const QuestionBox = styled.div`
+  align-self: center;
+  border-width: 2px;
+  margin: auto;
+  width: 60vh;
+  border: 2px solid #f5f5ef;
+`
+QuestionBox.displayName = 'StyledQuestionBox'
+
+export default QuestionBox
