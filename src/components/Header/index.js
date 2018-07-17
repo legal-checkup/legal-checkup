7ff8d83abd602acbfc95bf48ff07dfaa06022d71
YWHo <YWHo@users.noreply.github.com>
2018-07-17 15:43:02 +1200

    basic header with nav buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,21 +2,44 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledNavButton = styled.button`
-  color: #ff0000;
+  color: red;
   font-size: 16px;
-  margin: 4px 10px;
+  border: none;
+  padding: 4px 10px;
+  background-color: pink;
+  margin: 2px;
 `
-
 StyledNavButton.defaultName = 'StyledNavButton'
+
+const StyledHeader = styled.div`
+  display: flex;
+  fex-direction: horizontal;
+  justify-content: space-between;
+  background-color: purple;
+  padding: 10px;
+`
+StyledHeader.defaultName = 'StyledHeader'
 
 export default function Header () {
   return (
     <header>
-      <h1>Header</h1>
-      <h2>hello world!</h2>
-      <StyledNavButton>page1</StyledNavButton>
-      <StyledNavButton>page2</StyledNavButton>
-      <StyledNavButton>page3</StyledNavButton>
+      <StyledHeader>
+        <StyledNavButton>Home</StyledNavButton>
+        <div>
+          <StyledNavButton>page1</StyledNavButton>
+          <StyledNavButton>page2</StyledNavButton>
+          <StyledNavButton>page3</StyledNavButton>
+        </div>
+      </StyledHeader>
     </header>
   )
 }
+
+// function getListOfLinks() {
+//   return [
+//     {label: "page1",
+//      link: "#"},
+//     {},
+//     {},
+//   ]
+// }
