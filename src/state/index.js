6746a7c35ccda16295f8a291edda5f241391cd28
Reuben Berghan <reuben.berghan@gmail.com>
2018-08-04 16:05:26 +1200

    Updated state index for NavButton actions, constants, and selectors

--- src/state/index.js
+++ src/state/index.js
@@ -1,12 +1,23 @@
-import { testCount } from './actions'
-import { INITIAL_STATE, TEST_COUNT } from './constants'
+import { nextQuestion, previousQuestion, testCount } from './actions'
+import {
+  INITIAL_STATE,
+  NEXT_QUESTION,
+  PREVIOUS_QUESTION,
+  TEST_COUNT
+} from './constants'
 import rootEpic from './epics'
 import { rootReducer } from './reducers'
-import { getTestCount } from './selectors'
+import { getActiveQuestion, getCount, getTestCount } from './selectors'
 
 export {
+  getActiveQuestion,
+  getCount,
   getTestCount,
   INITIAL_STATE,
+  NEXT_QUESTION,
+  nextQuestion,
+  PREVIOUS_QUESTION,
+  previousQuestion,
   rootEpic,
   rootReducer,
   TEST_COUNT,
