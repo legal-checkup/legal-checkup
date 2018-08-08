eff6e65916b7f0c6827c5c40029e6eaebc41d832
Reuben Berghan <reuben.berghan@gmail.com>
2018-08-08 21:52:52 +1200

    Added selector for getCurrentTopic

--- src/state/selectors/index.js
+++ src/state/selectors/index.js
@@ -6,6 +6,10 @@ export function getCount ({ count }) {
   return count
 }
 
+export function getCurrentTopic ({ activeQuestion, questions = {} }) {
+  return questions[activeQuestion] && questions[activeQuestion].topic
+}
+
 export function getTestCount ({ rootReducer: { count } = {} }) {
   return count
 }
--- src/state/selectors/index.spec.js
+++ src/state/selectors/index.spec.js
@@ -1,15 +1,15 @@
-import { getActiveQuestion, getCount, getTestCount } from '.'
+import { getActiveQuestion, getCount, getCurrentTopic, getTestCount } from '.'
 
 describe('state:selectors', () => {
   describe('getTestCount', () => {
-    it('returns the list of completed badges', () => {
+    it('should return the list of completed badges', () => {
       const count = 3
       const state = { rootReducer: { count } }
 
       expect(getTestCount(state)).toBe(count)
     })
 
-    it('returns undefined if no count value', () => {
+    it('should return undefined if no count value', () => {
       const state = {}
 
       expect(getTestCount(state)).toBeUndefined()
@@ -17,14 +17,14 @@ describe('state:selectors', () => {
   })
 
   describe('getActiveQuestion', () => {
-    it('returns the active question', () => {
+    it('should return the active question', () => {
       const activeQuestion = 1
       const state = { activeQuestion }
 
       expect(getActiveQuestion(state)).toBe(activeQuestion)
     })
 
-    it('returns undefined if no activeQuestion value', () => {
+    it('should return undefined if no activeQuestion value', () => {
       const state = {}
 
       expect(getActiveQuestion(state)).toBeUndefined()
@@ -32,17 +32,38 @@ describe('state:selectors', () => {
   })
 
   describe('getCount', () => {
-    it('returns the count of answered questions', () => {
+    it('should return the count of answered questions', () => {
       const count = 1
       const state = { count }
 
       expect(getCount(state)).toBe(count)
     })
 
-    it('returns undefined if no count value', () => {
+    it('should return undefined if no count value', () => {
       const state = {}
 
       expect(getCount(state)).toBeUndefined()
+    })
+  })
+
+  describe('getCurrentTopic', () => {
+    it('should return undefined if no activeQuestion value', () => {
+      const state = {}
+
+      expect(getCurrentTopic(state)).toBeUndefined()
+    })
+
+    it('should return undefined if no question topic value', () => {
+      const state = { activeQuestion: 1, questions: { 1: { body: 'test' } } }
+
+      expect(getCurrentTopic(state)).toBeUndefined()
+    })
+
+    it('should return the topic of the activeQuestion', () => {
+      const topic = 'Money'
+      const state = { activeQuestion: 1, questions: { 1: { topic } } }
+
+      expect(getCurrentTopic(state)).toBe(topic)
     })
   })
 })
