86cb76c318e7c65293f4fd2c7aae6528e7b54390
Reuben Berghan <reuben.berghan@gmail.com>
2018-08-11 21:54:19 +1200

    Add test for StyledNavButton onClick pro logic

--- src/components/styled/NavButton/index.spec.js
+++ src/components/styled/NavButton/index.spec.js
@@ -7,4 +7,10 @@ describe('components:styled:NavButton', () => {
   it('renders the NavButton and CSS properties properly', () => {
     expect(toJson(shallow(<NavButton>&#x25C0;</NavButton>))).toMatchSnapshot()
   })
+
+  it('renders the NavButton and CSS properties properly with onClick prop', () => {
+    expect(
+      toJson(shallow(<NavButton onClick={() => null}>&#x25C0;</NavButton>))
+    ).toMatchSnapshot()
+  })
 })
