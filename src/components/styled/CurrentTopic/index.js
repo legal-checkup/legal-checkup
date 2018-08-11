9ef5752536acdfd389327a0aa992db013fd756e7
Reuben Berghan <reuben.berghan@gmail.com>
2018-08-11 17:12:43 +1200

    Added CurrentTopic styles

--- src/components/styled/CurrentTopic/index.js
+++ src/components/styled/CurrentTopic/index.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components'
 
-const CurrentTopic = styled.div``
+const CurrentTopic = styled.div`
+  background-color: #ededed;
+  color: #000000;
+  font-weight: bold;
+  height: 42px;
+  line-height: 42px;
+  text-align: center;
+  width: 191px;
+`
 
 CurrentTopic.displayName = 'StyledCurrentTopic'
 
