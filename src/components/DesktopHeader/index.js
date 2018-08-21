4745a973b07f334d2f08daa720565232e48d7b78
Carla <carlacfw@gmail.com>
2018-08-21 14:23:38 +1200

    styled component

---
+++ src/components/DesktopHeader/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import StyledDesktopHeader from './style'
+
+export default function DesktopHeader () {
+  return (
+    <StyledDesktopHeader>
+      <p>hi</p>
+    </StyledDesktopHeader>
+  )
+}
---
+++ src/components/DesktopHeader/style/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const StyledDesktopHeader = styled.div`
+  width: 1440px;
+  height: 100px;
+  background-color: #202020;
+`
+StyledDesktopHeader.displayName = 'StyledDesktopHeader'
+
+export default StyledDesktopHeader
