8a942d175e69bde00c6176f55abba12135bcc887
Priya Hiremath <priyahrish18@gmail.com>
2018-09-15 22:38:50 +1200

    fixed the snapshot Logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -11,21 +11,3 @@ export default function Logo () {
     </StyledLogo>
   )
 }
-
-// import React from 'react';
-// import { Link } from 'react-router';
-
-// class List extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <p>Please choose a repository from the list below.</p>
-//                 <ul>
-//                     <li><Link to="/react">React</Link></li>
-//                 </ul>
-//             </div>
-//         );
-//     }
-// }
-
-// export default List;
