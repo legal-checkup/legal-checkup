81b71bf58a643b9635ff77e5d1d4b8b898f99791
Carla <carlacfw@gmail.com>
2018-09-16 14:33:29 +1200

    update tests and testing containers

--- src/components/QuestionBox/index.spec.js
+++ src/components/QuestionBox/index.spec.js
@@ -1,45 +1,10 @@
 import React from 'react'
-
+import { shallow } from 'enzyme'
 import QuestionBox from '.'
-import { StyledQuestionBox } from '../styled'
 
 describe('components:QuestionBox', () => {
-  it(`renders the QuestionBox component when a question is provided`, () => {
-    const state = {
-      activeQuestion: 1,
-      questions: {
-        1: {
-          body: 'Is anyone chasing you for money?',
-          topic: 'Money Troubles',
-          help: 'The fact that someone is chasing you for money',
-          resource: 'credit and debt',
-          url:
-            'http://communitylaw.org.nz/community-law-manual/chapter-13-credit-and-debt/what-this-chapter-covers-chapter-13/'
-        }
-      }
-    }
-
-    expect(QuestionBox(state)).toEqual(
-      <StyledQuestionBox>
-        {'Is anyone chasing you for money?'}
-      </StyledQuestionBox>
-    )
-  })
-
-  it(`returns null when a question is not provided`, () => {
-    const state = {
-      activeQuestion: 1,
-      questions: {
-        1: {
-          topic: 'Money Troubles',
-          help: 'The fact that someone is chasing you for money',
-          resource: 'credit and debt',
-          url:
-            'http://communitylaw.org.nz/community-law-manual/chapter-13-credit-and-debt/what-this-chapter-covers-chapter-13/'
-        }
-      }
-    }
-
-    expect(QuestionBox(state)).toEqual(null)
-  })
+  it(`renders the QuestionBox component when a question is provided`, () =>
+    expect(
+      toJson(shallow(<QuestionBox>QuestionBox</QuestionBox>))
+    ).toMatchSnapshot())
 })
---
+++ src/containers/QuestionBox/index.spec.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
+
+import { getQuestionBody } from '../../state'
+import QuestionBox from '.'
+
+const mockStore = configureStore()
+
+describe('containers:QuestionBox', () => {
+  it('maps getQuestionBody to QuestionBox props', () => {
+    const body = 'Is anyone chasing you for money?'
+    const store = mockStore({ body: body })
+    const wrapper = shallow(<QuestionBox store={store} />)
+
+    expect(wrapper.props()).toEqual(
+      expect.objectContaining({
+        getQuestionBody
+      })
+    )
+  })
+})
