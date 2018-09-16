796aa48b25cf62385ae0a05c926f523986196a58
arist0cles <pattyboy_07@hotmail.com>
2018-09-16 13:22:06 +1200

    tests WIP

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -3,19 +3,11 @@ import {
   NOT_SURE_BUTTON_CLICKED,
   YES_BUTTON_CLICKED
 } from '../constants'
-function noButtonClicked () {
-  return {
-    type: NO_BUTTON_CLICKED
-  }
-}
-function yesButtonClicked () {
-  return {
-    type: YES_BUTTON_CLICKED
-  }
-}
-function notSureButtonClicked () {
-  return {
-    type: NOT_SURE_BUTTON_CLICKED
-  }
-}
+
+const noButtonClicked = () => ({ type: NO_BUTTON_CLICKED })
+
+const yesButtonClicked = () => ({ type: YES_BUTTON_CLICKED })
+
+const notSureButtonClicked = () => ({ type: NOT_SURE_BUTTON_CLICKED })
+
 export { noButtonClicked, yesButtonClicked, notSureButtonClicked }
--- src/state/reducers/index.spec.js
+++ src/state/reducers/index.spec.js
@@ -23,8 +23,44 @@ describe('state:reducers', () => {
         responses: { 1: 'NO' }
       }
 
-      expect(rootReducer(beforeState, { type: 'NO_BUTTON_CLICKED' })).toBe(
-        afterState
+      const NO_BUTTON_CLICKED = { type: 'NO_BUTTON_CLICKED' }
+
+      expect(JSON.stringify(rootReducer(beforeState, NO_BUTTON_CLICKED))).toBe(
+        JSON.stringify(afterState)
+      )
+    })
+
+    it('increases the activeQuestion when NOT_SURE_BUTTON_CLICKED', () => {
+      const beforeState = {
+        activeQuestion: 1
+      }
+
+      const afterState = {
+        activeQuestion: 2,
+        responses: { 1: 'NOT_SURE' }
+      }
+
+      const NOT_SURE_BUTTON_CLICKED = { type: 'NOT_SURE_BUTTON_CLICKED' }
+
+      expect(
+        JSON.stringify(rootReducer(beforeState, NOT_SURE_BUTTON_CLICKED))
+      ).toBe(JSON.stringify(afterState))
+    })
+
+    it('increases the activeQuestion when YES_BUTTON_CLICKED', () => {
+      const beforeState = {
+        activeQuestion: 1
+      }
+
+      const afterState = {
+        activeQuestion: 2,
+        responses: { 1: 'YES' }
+      }
+
+      const YES_BUTTON_CLICKED = { type: 'YES_BUTTON_CLICKED' }
+
+      expect(JSON.stringify(rootReducer(beforeState, YES_BUTTON_CLICKED))).toBe(
+        JSON.stringify(afterState)
       )
     })
   })
