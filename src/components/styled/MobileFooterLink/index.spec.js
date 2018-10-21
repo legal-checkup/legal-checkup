b2df29382e4b3c88599ac87cfa9b4334ac95f073
Carla <carlacfw@gmail.com>
2018-10-21 13:25:24 +1300

    add style snapshot tests

--- src/components/styled/MobileFooterLink/index.spec.js
+++ src/components/styled/MobileFooterLink/index.spec.js
@@ -4,6 +4,6 @@ import { shallow } from 'enzyme'
 import MobileFooterLink from '.'
 
 describe('components:styled:MobileFooterLink', function () {
-  it('renders the MobileFooter and CSS properties properly', () =>
+  it('renders the MobileFooterLink and CSS properties properly', () =>
     expect(toJson(shallow(<MobileFooterLink />))).toMatchSnapshot())
 })
