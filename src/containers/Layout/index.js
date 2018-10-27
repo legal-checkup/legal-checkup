9218f43476c1e2b4aa08197940c5a521e87f6869
ullibodnar <ullikins@gmail.com>
2018-10-27 21:36:07 +1300

    Add tests

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,9 +1,6 @@
 import React, { Fragment } from 'react'
 
-import { Desktop, Tablet, Mobile, Default } from '@responsive'
-
 import Header from '../Header'
-import DesktopHeader from '@containers/DesktopHeader'
 import Footer from '@components/Footer'
 import { Helmet } from 'react-helmet'
 import StyledLayout from '@components/styled/Layout'
@@ -11,45 +8,12 @@ import StyledLayout from '@components/styled/Layout'
 export default function Layout ({ children }) {
   return (
     <Fragment>
-      <Desktop>
-        <Helmet>
-          <title>Legal Checkup</title>
-        </Helmet>
-
-        <DesktopHeader />
-        <StyledLayout>{children}</StyledLayout>
-        <Footer />
-      </Desktop>
-
-      <Tablet>
-        <Helmet>
-          <title>Legal Checkup</title>
-        </Helmet>
-
-        <Header />
-        <StyledLayout>{children}</StyledLayout>
-        <Footer />
-      </Tablet>
-
-      <Mobile>
-        <Helmet>
-          <title>Legal Checkup</title>
-        </Helmet>
-
-        <Header />
-        <StyledLayout>{children}</StyledLayout>
-        <Footer />
-      </Mobile>
-
-      <Default>
-        <Helmet>
-          <title>Legal Checkup</title>
-        </Helmet>
-
-        <Header />
-        <StyledLayout>{children}</StyledLayout>
-        <Footer />
-      </Default>
+      <Helmet>
+        <title>Legal Checkup</title>
+      </Helmet>
+      <Header />
+      <StyledLayout>{children}</StyledLayout>
+      <Footer />
     </Fragment>
   )
 }
