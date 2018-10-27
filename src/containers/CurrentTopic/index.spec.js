13a7105d0e4376591d3130d6179ee27189c54be4
Patrick <patrick_t_ryan@bnz.co.nz>
2018-10-27 16:53:53 +1300

    WIP tests

--- src/containers/CurrentTopic/index.spec.js
+++ src/containers/CurrentTopic/index.spec.js
@@ -5,8 +5,19 @@ import { shallow } from 'enzyme'
 
 const mockStore = configureStore()
 
+const initialState = {
+  activeQuestion: {
+    topic: 'test'
+  }
+}
+
 describe('containers:CurrentTopic', () => {
-  it('should map currentTopic to props')
+  it('should map currentTopic to props', () => {
+    let store = mockStore(initialState)
+    const wrapper = shallow(<CurrentTopic store={store} />)
+
+    // expect(wrapper.props().children).toBe('test')
+  })
 
   it('should set currentTopic prop to topic of activeQuestionIndex from state')
 })
