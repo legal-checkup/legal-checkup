43e9d2659192c2bf50dc0614eb32ea4a625150a4
ullibodnar <ullikins@gmail.com>
2018-10-31 22:11:33 +1300

    Add tests to ResultsHero component

--- src/containers/ResultsHero/index.js
+++ src/containers/ResultsHero/index.js
@@ -13,8 +13,6 @@ function ResultsHero ({ yesCount }) {
           : `Based on your answers, it looks like you're in good legal health! 
         But if you have a problem, or something worrying you that wasn't covered by the questions 
         in this check-up, do come talk to us at Community Law`}
-
-        {console.log('YesCount:', yesCount)}
       </p>
       {yesCount > 0 ? <a href='#'>expand</a> : null}
       {yesCount > 0 ? (
--- src/containers/ResultsHero/index.spec.js
+++ src/containers/ResultsHero/index.spec.js
@@ -3,27 +3,39 @@ import { shallow } from 'enzyme'
 import configureStore from 'redux-mock-store'
 
 import { state } from '@state/fixtures'
+import { NO } from '@state/constants'
 
 const mockStore = configureStore()
 
+const stateWithZeroYesAnswers = {
+  responses: [
+    {
+      questions: [
+        {
+          answer: NO
+        }
+      ]
+    }
+  ]
+}
+
 const store = mockStore(state)
+const store2 = mockStore(stateWithZeroYesAnswers)
 
 import ResultsHero from '.'
 
 describe(`containers:ResultsHero`, () => {
   describe(`ResultsHero`, () => {
-    it(`should display expand controller when yesCount > 0`, () => {
-      expect(toJson(shallow(<ResultsHero store={store} />))).toMatchSnapshot()
-    })
-
-    it(`should display ResultsHero`, () => {
+    it(`should render ResultsHero properly when yesCount > 0 `, () => {
       expect(
         toJson(shallow(<ResultsHero store={store} />).dive())
       ).toMatchSnapshot()
     })
 
-    it(`should map yesCount to ResultsHero props`, () => {
-      expect(shallow(<ResultsHero store={store} />).props().yesCount).toBe(1)
+    it(`should render ResultsHero properly when yesCount == 0`, () => {
+      expect(
+        toJson(shallow(<ResultsHero store={store2} />).dive())
+      ).toMatchSnapshot()
     })
   })
 
