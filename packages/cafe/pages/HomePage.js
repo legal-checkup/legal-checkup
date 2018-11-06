7de5c888e355183d9309effa5929a31bc5488721
Charles F. Munat <chas@munat.com>
2018-11-06 21:00:35 +1300

    Add new test pages

--- packages/cafe/pages/HomePage.js
+++ packages/cafe/pages/HomePage.js
@@ -4,17 +4,24 @@ class HomePage {
   constructor() {
     this.footerCopyRight=Selector('.LinkExternalMobile-sc-1suge14-0')
     this.privacyPolicyPage=Selector('.MobileFooterLink-jjajg-0')
+    this.clickCheckup=Selector('#root > header > a:nth-child(2)')
     // this.termsPage = Selector('MobileFooterLink-jjajg-0');
   }
 
   async goToPrivacyPolicyPage() {
-    await t.click(this.privacyPolicyPage)
+    await t
+      .click(this.privacyPolicyPage)
   }
 
   async goToTermsOfUsePage() {
     await t
     // this.termsPage = termsPage.innerText.eq('Terms of Use')
     // .click(this.termsPage)
+  }
+
+  async gotoCheckupPage() {
+    await t
+      .click(this.clickCheckup)
   }
 }
 
