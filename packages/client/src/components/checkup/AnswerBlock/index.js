859983ff2470472cd6659b42cc2b2e4ab4c0588b
arist0cles <pattyboy_07@hotmail.com>
2018-12-02 13:55:27 +1300

    WIP adding test ids

--- packages/client/src/components/checkup/AnswerBlock/index.js
+++ packages/client/src/components/checkup/AnswerBlock/index.js
@@ -4,12 +4,13 @@ import { NO, NOT_SURE, YES } from '../../../state/constants'
 
 import Bar from './Bar'
 import ResponseButton from './ResponseButton'
+import { btnYes, btnNo } from '../../../constants'
 
 export default function AnswerBlock () {
   return (
     <Bar>
-      <ResponseButton type={YES} />
-      <ResponseButton type={NO} />
+      <ResponseButton data-testId={btnYes} type={YES} />
+      <ResponseButton data-testId={btnNo} type={NO} />
       <ResponseButton type={NOT_SURE} />
     </Bar>
   )
--- packages/client/src/constants.js
+++ packages/client/src/constants.js
@@ -2,3 +2,7 @@ export const DESKTOP = 'desktop'
 export const MOBILE = 'mobile'
 export const TABLET = 'tablet'
 export const WEBSITE = 'Legal Checkup'
+
+// Identifiers for testing
+export const btnYes = 'btnYes'
+export const btnNo = 'btnNo'
